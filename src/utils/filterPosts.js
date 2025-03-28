4f314ece594484b83034f76f028a9a48a2ebd87f
Foysal Hasan <foysal3909@gmail.com>
2025-03-28 12:57:21 +0600

    Date filter improvement done

--- src/utils/filterPosts.js
+++ src/utils/filterPosts.js
@@ -2,22 +2,55 @@ const filterPosts = (posts, filters = {}, limit = null) => {
   const { startDate, endDate, riskLevel, source } = filters;
 
   const filtered = posts.filter((post) => {
-    // if (startDate && new Date(post.date) <= new Date(startDate)) return false;
+    const postDate = new Date(post.date);
+
+    const postDateAtMidnight = new Date(postDate);
+    postDateAtMidnight.setHours(0, 0, 0, 0);
 
     if (startDate) {
       const startDateObj = new Date(startDate);
-      startDateObj.setDate(startDateObj.getDate() - 1);
-      if (new Date(post.date) <= startDateObj) return false;
+      startDateObj.setHours(0, 0, 0, 0);
+      if (postDateAtMidnight < startDateObj) return false;
     }
 
     if (endDate) {
       const endDateObj = new Date(endDate);
-      endDateObj.setDate(endDateObj.getDate() + 1);
-      if (new Date(post.date) >= endDateObj) return false;
+      endDateObj.setHours(0, 0, 0, 0);
+      if (postDateAtMidnight > endDateObj) return false;
     }
+
+    // if (startDate && new Date(post.date) <= new Date(startDate)) return false;
+
+    // if (startDate) {
+    //   const startDateObj = new Date(startDate);
+    //   startDateObj.setDate(startDateObj.getDate() - 1);
+    //   if (new Date(post.date) <= startDateObj) return false;
+    // }
+
+    // if (endDate) {
+    //   const endDateObj = new Date(endDate);
+    //   endDateObj.setDate(endDateObj.getDate() + 1);
+    //   if (new Date(post.date) >= endDateObj) return false;
+    // }
     // console.log('post.date', post.date, 'endDate', endDate);
     // if (source === '') {
     //   return true;
+    // }
+
+    // if (startDate) {
+    //   const startDateObj = new Date(startDate).toDateString();
+    //   const postDate = new Date(post.date).toDateString();
+    //   console.log('postDate', postDate, 'startDateObj', startDateObj);
+    //   console.log('postDate <= startDateObj', postDate <= startDateObj);
+    //   console.log('======================');
+    //   if (postDate <= startDateObj) return false;
+    // }
+
+    // if (endDate) {
+    //   const endDateObj = new Date(endDate).toDateString();
+    //   const postDate = new Date(post.date).toDateString();
+    //   // console.log('postDate', postDate, 'endDateObj', endDateObj);
+    //   if (postDate >= endDateObj) return false;
     // }
 
     if (source && post.source !== source) return false;
